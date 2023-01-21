09409782e97b52e213a5c02bd1bf9376c167fe25
Kamil Krauze <kamila.p.krauze@gmail.com>
2023-01-21 18:58:44 +0000

    PHP template reupload

--- template.php
+++ template.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
+    <?php
+    // Server based cookies, will come in handy when implementing QoL features, better done at the start
+    session_start();
+    ?>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
