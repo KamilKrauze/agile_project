13b3f81ebfdaf691ed9d39b971af66bbd2e940c4
veraborvinski <72395680+veraborvinski@users.noreply.github.com>
2023-01-23 17:47:03 +0000

    Add files via upload

---
+++ recipe.php
@@ -0,0 +1,29 @@
+<html>
+<body>
+
+<?php include 'header.php';?>
+    
+    <!-- Main container -->
+    <div class="main-body">
+        <!-- Do fancy code to get recipe title and info here-->
+    	<?php 
+            echo '<h1>Recipe title</h1>';
+            echo '<h2>Ingredients</h2>';
+            echo '<ol>
+                  <li>Coffee</li>
+                  <li>Tea</li>
+                  <li>Milk</li>
+                </ol>';
+            echo '<h2>Instructions</h2>';
+            echo '<ol>
+                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+                  <li>Eros in cursus turpis massa tincidunt dui ut. </li>
+                  <li>Vel quam elementum pulvinar etiam non.</li>
+                </ol>';
+        ?>
+    </div>
+
+<?php include 'footer.php';?>
+
+</body>
+</html>
