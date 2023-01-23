05eb273e3f24517c5050f8f61f9ec61836c246b0
Kamil Krauze <kamila.p.krauze@gmail.com>
2023-01-23 16:34:15 +0000

    Added back missing footer content
    Added back missing content to the footer

--- footer.php
+++ footer.php
@@ -1,12 +1,15 @@
-<footer class="bg-grey px-3" id="footer">
-    <div class="container-fluid">
-        <div class="row mt-2">
-            <div>
-                <p>Created by: </p>
-                <p>&copy Andrii Sultanov, Daryna Hnatenko, Dimitar Valkov, Kamil Krauze, Maria Mara Gatillo, Vera Borvinski</p>
+<footer class="footer bg-grey px-2" id="footer">
+            <div class="container-fluid">
+                <div class="row mt-2">
+                    <div class="col-sm-12 col-md-6">
+                        <p>Created by: </p>
+                        <p>&copy Andrii Sultanov, Daryna Hnatenko, Dimitar Valkov, Kamil Krauze, Maria Mara Gatillo, Vera Borvinski</p>
+                    </div>
+                    <div class="col-sm-12 col-md-6 justify-content-xs-start justify-content-md-end">
+                        <p>Some copyright content</p>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</footer>
+        </footer>
 
 </html>
