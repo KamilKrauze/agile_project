fa18b0aeba6fabde927fe047cf5fd0271e34e181
gatillom <35645373+gatillom@users.noreply.github.com>
2023-01-24 23:26:18 +0000

    Added sharing text and format

--- recipe.php
+++ recipe.php
@@ -24,6 +24,22 @@
         <h3>Allergy Disclaimer: LGL's products are plastic free and we can thus not ensure that cross-contamination has not occured.</h3>
     </div>
 
+        <!-- Sharing Alert -->
+        <div class="alert alert-success" role="alert">
+          Share this recipe!
+    
+        <!-- Sharing Links -->
+        <!-- Twitter Share Button -->
+        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false" data-inline="true">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    
+        <!-- Facebook Share Button -->
+        <id="fb-root">
+        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v15.0" nonce="i5TxLOMx"></script>
+        
+        <!-- link placeholder -->
+        <div class="fb-share-button" data-href="https://agile-team1.azurewebsites.net/index.php" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fagile-team1.azurewebsites.net%2Findex.php&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
+        </div>
+    
 <?php include 'footer.php';?>
 
 </body>
