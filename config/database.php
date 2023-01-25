95b824cf0673581d0b62531fa36ca5c8646bbc9b
Kamil Krauze <kamila.p.krauze@gmail.com>
2023-01-25 13:21:48 +0000

    DB Connection
    All tested using PDO + SSL
    
    Co-Authored-By: DimitarValkov2113 <72396360+DimitarValkov2113@users.noreply.github.com>

--- config/database.php
+++ config/database.php
@@ -1,3 +1,23 @@
 <?php
-// Connection to MySQL server hosted on Azure
+$host =  "agile-team1.mysql.database.azure.com"; // Hostname
+$username = "agileteam1-u01";
+$password = "u01-ag1*";
+$db_name = "ldl"; // The default schema you should be using
+$port = '3306';
+$options = array(
+    PDO::MYSQL_ATTR_SSL_CA => "../ssl/certs/DigiCertGlobalRootCA.crt.pem",
+    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+);
+
+$dsn = "mysql:host=$host;dbname=$db_name;port=$port";
+
+try {
+    $pdo = new PDO($dsn ,$username, $password, $options);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    include './errors/connection_failed.php';
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+
 ?>
