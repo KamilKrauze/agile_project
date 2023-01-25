9ca7c825c6c0f0032c4600369e2449791201592f
Kamil Krauze <kamila.p.krauze@gmail.com>
2023-01-25 18:11:20 +0000

    Footer code formatting fix

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 <footer class="footer bg-grey px-2" id="footer">
-            <div class="container-fluid">
-                <div class="row mt-2">
-                    <div class="col-sm-12 col-md-6">
-                        <p>Created by: </p>
-                        <p>&copy Andrii Sultanov, Daryna Hnatenko, Dimitar Valkov, Kamil Krauze, Maria Mara Gatillo, Vera Borvinski</p>
-                    </div>
-                    <div class="col-sm-12 col-md-6 justify-content-xs-start justify-content-md-end">
-                        <p>Some copyright content</p>
-                    </div>
-                </div>
+    <div class="container-fluid">
+        <div class="row mt-2">
+            <div class="col-sm-12 col-md-6">
+                <p>Created by: </p>
+                <p>&copy Andrii Sultanov, Daryna Hnatenko, Dimitar Valkov, Kamil Krauze, Maria Mara Gatillo, Vera Borvinski</p>
             </div>
-        </footer>
+            <div class="col-sm-12 col-md-6 justify-content-xs-start justify-content-md-end">
+                <p>Some copyright content</p>
+            </div>
+        </div>
+    </div>
+</footer>
 
 </html>
