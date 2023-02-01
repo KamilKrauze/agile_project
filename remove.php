abdd1190d272c62a94b765019dc88051b12b787c
veraborvinski <72395680+veraborvinski@users.noreply.github.com>
2023-02-01 15:03:34 +0000

    Added removal for ingredients
    can't get the login page to work so had to comment out while testing

--- remove.php
+++ remove.php
@@ -1,7 +1,7 @@
 <?php
     include '../config/database.php';
-    if(isset($_GET['id'])) {
-        $rid = $_GET['id'];
+    if(isset($_GET['rid'])) {
+        $rid = $_GET['rid'];
         $dfr = $pdo->prepare("DELETE FROM Recipes WHERE RecipeID=?");
         $dfr->bindParam(1, $rid, PDO::PARAM_INT);
         $dfr->execute();
@@ -12,7 +12,20 @@
 
         include "search.php";
     }
+    else if(isset($_GET['iid'])) {
+        $iid = $_GET['iid'];
+        $dfr = $pdo->prepare("DELETE FROM ingredients WHERE ingredientID=?");
+        $dfr->bindParam(1, $iid, PDO::PARAM_INT);
+        $dfr->execute();
+
+        $dfr = $pdo->prepare("DELETE FROM recipeingredients WHERE ingredientID=?");
+        $dfr->bindParam(1, $iid, PDO::PARAM_INT);
+        $dfr->execute();
+
+        include "search.php";
+    }
     else{
         //no id wompwomp:(
+        include "search.php";
     }
 ?>
