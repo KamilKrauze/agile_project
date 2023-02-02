6d7f0acfd0893b2025155414ceee80c549412294
DimitarValkov <dimitarvalkov92@gmail.com>
2023-02-02 23:11:32 +0000

    modified it to work with ajax calls and clear localstorage so that it is more robust

--- admin/js/search_buttons.js
+++ admin/js/search_buttons.js
@@ -2,17 +2,23 @@ function editItem(element, itemID, itemType) {
     console.log(element);
     console.log(itemType);
     console.log(itemID);
-
-    $.post("modify.php", {
+    $.ajax({
+        url: 'modify.php',
+        type: 'POST',
+        async: false,
+        data: {
         op_type: "edit",
         item_type : itemType,
-        item_id : String(itemID),
-    } , (response) => {localStorage.setItem("modifyHTML", JSON.stringify(response));
+        item_id : String(itemID)},
+        success: (response) => {localStorage.setItem("modifyHTML", JSON.stringify(response));
             window.location.href = "modify.php";}
-    );
+    });
 }
 window.onbeforeunload = () => {
     console.log("Changing page!");
+}
+window.onload = function clearLocalStorage(){
+    localStorage.clear();
 }
 function remItem(element, itemID, itemType) {
     console.log(element);
